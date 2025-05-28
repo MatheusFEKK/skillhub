87f9e1135b85877733c8a9facdc2366054f2a51f
Samuel Moreira Furtado <samuelmoreirafurtado@gmail.com>
2025-05-28 16:44:36 -0300

    Fidelidade da pagina de autentificação

--- src/components/ButtonGroup.tsx
+++ src/components/ButtonGroup.tsx
@@ -12,7 +12,6 @@ interface ButtonGroupProps{
 export const ButtonGroup:React.FC<ButtonGroupProps> = (props) => {
     return(
         <View style={[styles.flexDirectionRow]}>
-
             <TouchableOpacity style={[styles.buttonDefaultMin, styles.alignItemsCenter, styles.justifyContentCenter, (props.Activated == 0) ? {borderBottomColor:'#54A7F4'}: {borderBottomColor:'#C3C8D7'}]} onPress={() => props.functionButton1(0)}>
                 <Text style={[styles.fontSize4, styles.fontWeightMedium]}>{props.textButton1}</Text>
             </TouchableOpacity>
