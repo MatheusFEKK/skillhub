ecd7b00918521c6e965580a673305a5d9897cbae
Samuel Moreira Furtado <samuelmoreirafurtado@gmail.com>
2025-06-03 15:24:43 -0300

    Criação do ProfileSystem

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -6,7 +6,8 @@ export const styles = StyleSheet.create({
         flex: 1
     },
     splashRoot:{
-        flex: 1
+        flex: 1,
+        backgroundColor: 'transparent'
     },
     card: {
         flexDirection: "row",
